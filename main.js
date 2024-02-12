55b5bbb27654fabe093b7633a6d50091891a795b
Prisha Shreevastav <112982045+PrishaS2007@users.noreply.github.com>
2024-02-12 11:24:01 -0700

    added js

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 // Triangle Perimeter Assignment Start Code
-document.getElementById('btn').addEventListener('click', btnClicked);
+document.getElementById("btn").addEventListener("click", btnClicked)
+
+btnClicked() {
+    
+}
+
+let calcDist = Math.sqrt((x2 - x1)** 2 + (y2 - y1) ** 2); 
+function dist(x1, y1, x2, y2) {
+
+}
