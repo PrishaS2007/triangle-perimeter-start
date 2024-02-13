5361f93cdb94a90233e47e5f121be6211edf0d18
Prisha Shreevastav <112982045+PrishaS2007@users.noreply.github.com>
2024-02-13 10:36:07 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 // Triangle Perimeter Assignment Start Code
 document.getElementById("btn").addEventListener("click", btnClicked)
 
+// Input
 function btnClicked() {
-let AB
+let x1In = +document.getElementById("x1").value;
+let x2In = +document.getElementById("x2").value;
+let x3In = +document.getElementById("x3").value;
+let y1In = +document.getElementById("y1").value;
+let y2In = +document.getElementById("y2").value;
+let y3In = +document.getElementById("y3").value;
 }
+
+// Process
+let AB = dist(x1In, y1In, x2In, y2In);
+let AC = dist(x1In, y1In, x3In, y3In);
+let BC = dist(x2In, y2In, x3In, y3In);
+let Perimeter = (AB + AC + BC);
