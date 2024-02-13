15ccceac6b114c4e62ac9c293106e3cc86597a47
Prisha Shreevastav <112982045+PrishaS2007@users.noreply.github.com>
2024-02-13 10:53:39 -0700

    Update main.js

--- main.js
+++ main.js
@@ -9,10 +9,18 @@ let x3In = +document.getElementById("x3").value;
 let y1In = +document.getElementById("y1").value;
 let y2In = +document.getElementById("y2").value;
 let y3In = +document.getElementById("y3").value;
-}
 
 // Process
 let AB = dist(x1In, y1In, x2In, y2In);
 let AC = dist(x1In, y1In, x3In, y3In);
 let BC = dist(x2In, y2In, x3In, y3In);
 let Perimeter = (AB + AC + BC);
+}
+
+let calcDist = Math.sqrt((x2 - x1)** 2 + (y2 - y1) ** 2)
+
+
+
+function dist(x1In,y1In,x2In,y2In) {
+    let calcDistst = Math.sqrt((x2 - x1)** 2 + (y2 - y1) ** 2)
+}
