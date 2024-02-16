67d36d2a9958207047967ba9cd97a7adfa9ac920
Prisha Shreevastav <112982045+PrishaS2007@users.noreply.github.com>
2024-02-15 20:02:04 -0700

    Update main.js

--- main.js
+++ main.js
@@ -11,12 +11,16 @@ let y2In = +document.getElementById("y2").value;
 let y3In = +document.getElementById("y3").value;
 
 // Process
-let AB = dist(x1In, y1In, x2In, y2In);
-let AC = dist(x1In, y1In, x3In, y3In);
-let BC = dist(x2In, y2In, x3In, y3In);
+let AB = dist(x1In, x2In, y1In, y2In);
+let AC = dist(x1In, x3In, y1In, y3In);
+let BC = dist(x2In, x3In, y2In, y3In);
 let Perimeter = (AB + AC + BC);
+document.getElementById('output4').innerHTML = Perimeter
+document.getElementById('output1').innerHTML = AB
+document.getElementById('output3').innerHTML = BC
+document.getElementById('output2').innerHTML = AC
 }
 
-function dist(x1, y1, x2, y2) {
-return Math.sqrt((x2 - x1)*y1+ (y2 - y1) ** 2);
+function dist(x1, x2, y1, y2) {
+return Math.sqrt((x2 - x1)** 2 + (y2 - y1) ** 2);
 }
